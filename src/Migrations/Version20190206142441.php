c978e48f096b9660507e0749290524c60e6369a8
aziza <aaziza.pollet@gmail.com>
2019-02-06 15:31:00 +0100

    update

---
+++ src/Migrations/Version20190206142441.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190206142441 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE documents ADD category_id INT NOT NULL, ADD borrower_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE documents ADD CONSTRAINT FK_A2B0728812469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('ALTER TABLE documents ADD CONSTRAINT FK_A2B0728811CE312B FOREIGN KEY (borrower_id) REFERENCES borrower (id)');
+        $this->addSql('CREATE INDEX IDX_A2B0728812469DE2 ON documents (category_id)');
+        $this->addSql('CREATE INDEX IDX_A2B0728811CE312B ON documents (borrower_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE documents DROP FOREIGN KEY FK_A2B0728812469DE2');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('ALTER TABLE documents DROP FOREIGN KEY FK_A2B0728811CE312B');
+        $this->addSql('DROP INDEX IDX_A2B0728812469DE2 ON documents');
+        $this->addSql('DROP INDEX IDX_A2B0728811CE312B ON documents');
+        $this->addSql('ALTER TABLE documents DROP category_id, DROP borrower_id');
+    }
+}
