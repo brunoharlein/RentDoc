8db10c1c666809d178b6cf00a1e7267b2b960623
Bruno HARLEIN <bruno@harlein.com>
2019-02-19 09:04:45 +0100

    add readme.md

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,27 +2,29 @@
 
 namespace App\DataFixtures;
 
+use Faker;
+use Faker\Provider\Base;
 use App\Entity\Documents;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-        // for ($i = 0; $i < 20; $i++) {
-        //   $documents = new Documents();
-        //   $documents->setTitle ($faker->name);
-        //   $documents->setAuthor ($faker->name);
-        //   $documents->setReleaseDate ($faker->date($format = 'Y-m-d', $max = 'now'));
-        //   $documents->setStatu ($faker->boolean);
-        //   $documents->setResume ($faker->name);
-        //   $manager->persist($documents);
-        //
-        // }
-        //
-        // $manager->flush();
+      // $faker = Faker\Factory::create('fr_FR');
+      //
+      //     $documents = new Documents();
+      //     $documents->setTitle ($faker->name);
+      //     $documents->setAuthor ($faker->name);
+      //     $documents->setReleaseDate (new \DateTime($faker->date($format = 'Y-m-d', $max = 'now')));
+      //     $documents->setStatu ($faker->boolean);
+      //     $documents->setResume ($faker->text);
+      //
+      //     $manager->persist($documents);
+      //
+      //   $manager->flush();
     }
 }
